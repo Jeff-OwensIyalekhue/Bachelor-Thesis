517efb1ee6962b2a8da8cf3c3ad03c975fac8d93
Jeff-Owens Iyalekhue <j.oiyalekhue@hotmail.de>
2018-08-24 11:55:46 +0200

    base game implemented

---
+++ Bachelor-Thesis/Assets/Scripts/GameLogic.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.EventSystems;
+
+
+public class GameLogic : MonoBehaviour {
+
+    [SerializeField]
+    bool testing = false;
+
+    [Header("UI")]
+    public TMP_Text score;
+    public TMP_Text task;
+    public TMP_InputField inputField;
+    EventSystem eventSystem;
+
+    [Header("Settings")]
+    public int intervalMin = 0;
+    public int intervalMax = 10;    // exclusive
+
+    int numberA; 
+    int numberB;
+    int op;                         // operator: 0 := addition; 1 := subtraction; 2 := multiplication; else := error
+    [SerializeField]
+    int expResult;                  // expected result = numberA op numberB
+
+    int entResult;                  // entered rusult
+
+    bool answered = true;
+
+
+	// Use this for initialization
+	void Start () {
+        eventSystem = FindObjectOfType<EventSystem>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (answered || (testing && Input.GetKeyDown(KeyCode.Escape)))
+        {
+            Task();
+        }
+        if (!eventSystem.alreadySelecting)
+        {
+            eventSystem.SetSelectedGameObject(inputField.gameObject);
+        }
+	}
+
+    // Sets a new task up
+    void Task()
+    {
+        score.text = "Correct: " + GameManager.Instance.correctAnswers + " - Wrong:" + GameManager.Instance.wrongAnswers;
+
+        answered = false;
+        numberA = Random.Range(intervalMin, intervalMax);
+        numberB = Random.Range(intervalMin, intervalMax);
+        op = Random.Range(0, 3);
+
+        switch (op)
+        {
+            case 0:
+                expResult = numberA + numberB;
+                task.text = numberA + " <color=green> + </color> " + numberB;
+                break;
+            case 1:
+                expResult = numberA - numberB;
+                task.text = numberA + " <color=red> - </color> " + numberB;
+                break;
+            case 2:
+                expResult = numberA * numberB;
+                task.text = numberA + " <color=blue> * </color> " + numberB;
+                break;
+            default:
+                Debug.Log("unexpected value as operaor");
+                break;
+        }
+    }
+
+    public void Solve()
+    {
+        if (inputField.text != null)
+        {
+            entResult = int.Parse(inputField.text);
+            if (entResult == expResult)
+            {
+                GameManager.Instance.correctAnswers++;
+            }
+            else
+            {
+                GameManager.Instance.wrongAnswers++;
+            }
+        }
+        
+        inputField.DeactivateInputField();
+        inputField.text = "";
+        inputField.ActivateInputField();
+
+        Task();
+    }
+}
---
+++ Bachelor-Thesis/Assets/Scripts/GameManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public class GameManager{
+
+    #region Instanziierung
+    private static GameManager instance;
+
+    private GameManager()
+    {
+        if (instance != null)
+            return;
+        instance = this;
+
+    }
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new GameManager();
+            }
+
+            return instance;
+        }
+    }
+    #endregion
+
+    public int correctAnswers = 0;
+    public int wrongAnswers = 0;
+
+    public bool gameRunning = false;
+
+    public string input = "test";
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void SaveData()
+    {
+        BinaryFormatter bF = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/testData.txt");
+
+        SaveData data = new SaveData();
+        data.data = input;
+        
+        bF.Serialize(file, data);
+        file.Close();
+    }
+
+    public void LoadData()
+    {
+
+    }
+
+    // End the game / return to the desktop
+    public void EndGame()
+    {
+        //If we are running in a standalone build of the game
+        #if UNITY_STANDALONE
+            //Quit the application
+            Application.Quit();
+        #endif
+
+        //If we are running in the editor
+        #if UNITY_EDITOR
+            //Stop playing the scene
+            UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+}
+
+[Serializable]
+class SaveData
+{
+    public string data;
+}
